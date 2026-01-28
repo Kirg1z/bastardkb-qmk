ploopyco/qmk_firmware-trackpad

Rebase pavonis+peacock on develop

drivers/sensors/azoteq_iqs5xx.h
@@ -5,8 +5,21 @@
 #pragma once
 
 #include "i2c_master.h"
-#include "pointing_device.h"
 #include "util.h"
+
+#if defined(AZOTEQ_IQS5XX_TPS43)
+#    define AZOTEQ_IQS5XX_WIDTH_MM 43
+#    define AZOTEQ_IQS5XX_HEIGHT_MM 40
+#    define AZOTEQ_IQS5XX_RESOLUTION_X 2048
+#    define AZOTEQ_IQS5XX_RESOLUTION_Y 1792
+#elif defined(AZOTEQ_IQS5XX_TPS65)
+#    define AZOTEQ_IQS5XX_WIDTH_MM 65
+#    define AZOTEQ_IQS5XX_HEIGHT_MM 49
+#    define AZOTEQ_IQS5XX_RESOLUTION_X 3072
+#    define AZOTEQ_IQS5XX_RESOLUTION_Y 2048
+#elif !defined(AZOTEQ_IQS5XX_WIDTH_MM) && !defined(AZOTEQ_IQS5XX_HEIGHT_MM)
+#    error "You must define one of the available azoteq trackpads or specify at least the width and height"
+#endif
 
 typedef enum {
     AZOTEQ_IQS5XX_UNKNOWN,
@@ -66,7 +79,7 @@ typedef struct PACKED {
 typedef struct {
     uint8_t h : 8;
     uint8_t l : 8;
-} azoteq_iqs5xx_relative_xy_t;
+} azoteq_iqs5xx_xy_t;
 
 typedef struct {
     uint8_t                          previous_cycle_time;
@@ -75,19 +88,39 @@ typedef struct {
     azoteq_iqs5xx_system_info_0_t    system_info_0;
     azoteq_iqs5xx_system_info_1_t    system_info_1;
     uint8_t                          number_of_fingers;
-    azoteq_iqs5xx_relative_xy_t      x;
-    azoteq_iqs5xx_relative_xy_t      y;
+    azoteq_iqs5xx_xy_t               x;
+    azoteq_iqs5xx_xy_t               y;
 } azoteq_iqs5xx_base_data_t;
 
-_Static_assert(sizeof(azoteq_iqs5xx_base_data_t) == 10, "azoteq_iqs5xx_basic_report_t should be 10 bytes");
+_Static_assert(sizeof(azoteq_iqs5xx_base_data_t) == 10, "azoteq_iqs5xx_base_data_t should be 10 bytes");
 
 typedef struct {
-    uint8_t                     number_of_fingers;
-    azoteq_iqs5xx_relative_xy_t x;
-    azoteq_iqs5xx_relative_xy_t y;
+    uint8_t            number_of_fingers;
+    azoteq_iqs5xx_xy_t x;
+    azoteq_iqs5xx_xy_t y;
 } azoteq_iqs5xx_report_data_t;
 
 _Static_assert(sizeof(azoteq_iqs5xx_report_data_t) == 5, "azoteq_iqs5xx_report_data_t should be 5 bytes");
+
+typedef struct PACKED {
+    uint8_t h : 8;
+    uint8_t l : 8;
+} azoteq_iqs5xx_touch_strength_t;
+
+typedef struct PACKED {
+    azoteq_iqs5xx_xy_t             x;
+    azoteq_iqs5xx_xy_t             y;
+    azoteq_iqs5xx_touch_strength_t strength;
+    uint8_t                        touch_area;
+} azoteq_iqs5xx_absolute_finger_data_t;
+
+_Static_assert(sizeof(azoteq_iqs5xx_absolute_finger_data_t) == 7, "azoteq_iqs5xx_absolute_finger_data_t should be 7 bytes");
+
+typedef struct PACKED {
+    azoteq_iqs5xx_absolute_finger_data_t fingers[5];
+} azoteq_iqs5xx_digitizer_data_t;
+
+//_Static_assert(sizeof(azoteq_iqs5xx_digitizer_data_t) == 40, "azoteq_iqs5xx_digitizer_data_t should be 40 bytes");
 
 typedef struct PACKED {
     bool sw_input : 1;
@@ -178,7 +211,9 @@ typedef struct {
 
 void           azoteq_iqs5xx_init(void);
 i2c_status_t   azoteq_iqs5xx_wake(void);
+#ifdef POINTING_DEVICE_ENABLE
 report_mouse_t azoteq_iqs5xx_get_report(report_mouse_t mouse_report);
+#endif
 i2c_status_t   azoteq_iqs5xx_get_report_rate(azoteq_iqs5xx_report_rate_t *report_rate, azoteq_iqs5xx_charging_modes_t mode, bool end_session);
 i2c_status_t   azoteq_iqs5xx_set_report_rate(uint16_t report_rate_ms, azoteq_iqs5xx_charging_modes_t mode, bool end_session);
 i2c_status_t   azoteq_iqs5xx_set_event_mode(bool enabled, bool end_session);

Kirg1z/bastardkb-qmk

drivers/sensors/azoteq_iqs5xx.h
@@ -6,8 +6,21 @@
 
 #include "compiler_support.h"
 #include "i2c_master.h"
-#include "pointing_device.h"
 #include "util.h"
+
+#if defined(AZOTEQ_IQS5XX_TPS43)
+#    define AZOTEQ_IQS5XX_WIDTH_MM 43
+#    define AZOTEQ_IQS5XX_HEIGHT_MM 40
+#    define AZOTEQ_IQS5XX_RESOLUTION_X 2048
+#    define AZOTEQ_IQS5XX_RESOLUTION_Y 1792
+#elif defined(AZOTEQ_IQS5XX_TPS65)
+#    define AZOTEQ_IQS5XX_WIDTH_MM 65
+#    define AZOTEQ_IQS5XX_HEIGHT_MM 49
+#    define AZOTEQ_IQS5XX_RESOLUTION_X 3072
+#    define AZOTEQ_IQS5XX_RESOLUTION_Y 2048
+#elif !defined(AZOTEQ_IQS5XX_WIDTH_MM) && !defined(AZOTEQ_IQS5XX_HEIGHT_MM)
+#    error "You must define one of the available azoteq trackpads or specify at least the width and height"
+#endif
 
 typedef enum {
     AZOTEQ_IQS5XX_UNKNOWN,
@@ -67,7 +80,7 @@ typedef struct PACKED {
 typedef struct {
     uint8_t h : 8;
     uint8_t l : 8;
-} azoteq_iqs5xx_relative_xy_t;
+} azoteq_iqs5xx_xy_t;
 
 typedef struct {
     uint8_t                          previous_cycle_time;
@@ -76,19 +89,39 @@ typedef struct {
     azoteq_iqs5xx_system_info_0_t    system_info_0;
     azoteq_iqs5xx_system_info_1_t    system_info_1;
     uint8_t                          number_of_fingers;
-    azoteq_iqs5xx_relative_xy_t      x;
-    azoteq_iqs5xx_relative_xy_t      y;
+    azoteq_iqs5xx_xy_t               x;
+    azoteq_iqs5xx_xy_t               y;
 } azoteq_iqs5xx_base_data_t;
 
-STATIC_ASSERT(sizeof(azoteq_iqs5xx_base_data_t) == 10, "azoteq_iqs5xx_basic_report_t should be 10 bytes");
+STATIC_ASSERT(sizeof(azoteq_iqs5xx_base_data_t) == 10, "azoteq_iqs5xx_base_data_t should be 10 bytes");
 
 typedef struct {
-    uint8_t                     number_of_fingers;
-    azoteq_iqs5xx_relative_xy_t x;
-    azoteq_iqs5xx_relative_xy_t y;
+    uint8_t            number_of_fingers;
+    azoteq_iqs5xx_xy_t x;
+    azoteq_iqs5xx_xy_t y;
 } azoteq_iqs5xx_report_data_t;
 
 STATIC_ASSERT(sizeof(azoteq_iqs5xx_report_data_t) == 5, "azoteq_iqs5xx_report_data_t should be 5 bytes");
+
+typedef struct PACKED {
+    uint8_t h : 8;
+    uint8_t l : 8;
+} azoteq_iqs5xx_touch_strength_t;
+
+typedef struct PACKED {
+    azoteq_iqs5xx_xy_t             x;
+    azoteq_iqs5xx_xy_t             y;
+    azoteq_iqs5xx_touch_strength_t strength;
+    uint8_t                        touch_area;
+} azoteq_iqs5xx_absolute_finger_data_t;
+
+_Static_assert(sizeof(azoteq_iqs5xx_absolute_finger_data_t) == 7, "azoteq_iqs5xx_absolute_finger_data_t should be 7 bytes");
+
+typedef struct PACKED {
+    azoteq_iqs5xx_absolute_finger_data_t fingers[5];
+} azoteq_iqs5xx_digitizer_data_t;
+
+//_Static_assert(sizeof(azoteq_iqs5xx_digitizer_data_t) == 40, "azoteq_iqs5xx_digitizer_data_t should be 40 bytes");
 
 typedef struct PACKED {
     bool sw_input : 1;
@@ -182,7 +215,9 @@ extern const pointing_device_driver_t azoteq_iqs5xx_pointing_device_driver;
 
 bool           azoteq_iqs5xx_init(void);
 i2c_status_t   azoteq_iqs5xx_wake(void);
+#ifdef POINTING_DEVICE_ENABLE
 report_mouse_t azoteq_iqs5xx_get_report(report_mouse_t mouse_report);
+#endif
 i2c_status_t   azoteq_iqs5xx_get_report_rate(azoteq_iqs5xx_report_rate_t *report_rate, azoteq_iqs5xx_charging_modes_t mode, bool end_session);
 i2c_status_t   azoteq_iqs5xx_set_report_rate(uint16_t report_rate_ms, azoteq_iqs5xx_charging_modes_t mode, bool end_session);
 i2c_status_t   azoteq_iqs5xx_set_event_mode(bool enabled, bool end_session);